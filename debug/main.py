ed7091a68e4899154eeb52587901e1c94a9bfb5b
Gabriel Pietroluongo <gabrielpietroluongo@gmail.com>
2023-07-10 21:05:40 -0300

    Added more helpful message when failing to connect to mqtt server

--- debug/main.py
+++ debug/main.py
@@ -1,11 +1,15 @@
+import signal
 from gui.gui import draw_all
 from comms.mqtt_sender import Sender
-import signal
 
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
 def main():
-    _sender = Sender()
+    try:
+        _sender = Sender()
+    except Exception:
+        print("\033[91m\033[1m[ERROR]\033[0m\033[1m Failed to connect to MQTT Broker\033[0m")
+        return
     draw_all()
 
 
